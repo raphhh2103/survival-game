e800b807c7835e5456093b5a503564768ff4c9f8
raphaelk <raphaelk@live.be>
2020-06-11 18:41:44 +0200

    init commit

---
+++ Assets/Editor/WorldGeneratorEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WorldGenerator))]
+public class WorldGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        WorldGenerator mapGen = (WorldGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("generate"))
+        {
+            mapGen.GenerateMap();
+        }
+
+    }
+}
